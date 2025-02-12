c86f5d6c67bfac67495f5b184522717214cb3f57
choidahey <81176789+choidahey@users.noreply.github.com>
2025-02-12 09:45:06 +0900

    [level 1] Title: 모의고사, Time: 0.12 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,48 +1,35 @@
+// 각 학생별 찍는 방식 설정
+// answer 순회하면서 
+
 #include <string>
 #include <vector>
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 vector<int> solution(vector<int> answers) {
-    vector<int> score;
-    vector<int> result;
+    vector<int> answer;
+    vector<int> student1 = { 1, 2, 3, 4, 5 };
+    vector<int> student2 = { 2, 1, 2, 3, 2, 4, 2, 5 };
+    vector<int> student3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+    vector<int> count = { 0, 0, 0 };
     
-    vector<int> supo1 = {1, 2, 3, 4, 5};
-    vector<int> supo2 = {2, 1, 2, 3, 2, 4, 2, 5};
-    vector<int> supo3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-    
-    int score1 = 0;
-    int score2 = 0;
-    int score3 = 0;
-    
-    // 각 수포자 답과 answers 비교 - 총 3번
-    for (int i = 0; i < answers.size(); i++){
-        if (supo1[i % supo1.size()] == answers[i])  // 답의 갯수가 15개일 때 supo들의 답이 저장된 배열의 크기 때문에
-            score1++;
-        
-        if (supo2[i % supo2.size()] == answers[i])
-            score2++;
-        
-        if (supo3[i % supo3.size()] == answers[i])
-            score3++;
-        
-    }
-    score.push_back(score1);
-    score.push_back(score2);
-    score.push_back(score3);
-    
-    
-    // 가장 많은 문제를 맞춘 score 출력
-    int high_score = *max_element(score.begin(), score.end());
-    
-    // 문제 많이 맞춘 사람의 위치 출력
-    for (int k = 0; k < score.size(); k++){
-        if (score[k] == high_score){
-            result.push_back(k + 1);  // 1부터 출력하려고
-        }
+    for (int i = 0; i < answers.size(); i++)
+    {
+        if (answers[i] == student1[i%student1.size()]) { count[0]++; }
+        if (answers[i] == student2[i%student2.size()]) { count[1]++; }
+        if (answers[i] == student3[i%student3.size()]) { count[2]++; }
     }
     
-    return result;
+    int max_value = *max_element(count.begin(), count.end());
+    
+    for (int j = 0; j < count.size(); j++)
+    {
+        if (count[j] == max_value) { answer.push_back(j+1); }
+    }
+    
+    sort(answer.begin(), answer.end());
+    
+    return answer;
 }
